04c54460f97c2b7c1aafa76cc7873f9d89eb2d7b
Athu Kawale <atharvakawale63@gmail.com>
2023-08-22 12:35:59 +0530

    the features added and started to build the application, search bar added to app.js and errors of appregistary resolved

--- App.js
+++ App.js
@@ -2,17 +2,25 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import SafeViewAndroid from "./src/utils/SafeViewAndroid";
+import { Searchbar } from "react-native-paper";
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = React.useState(null);
+  const onChangeSearch = (query) => setSearchQuery(query);
   return (
     <>
       <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-        <View style={styles.container1}>
-          <Text>Search is here</Text>
+        <View style={styles.search}>
+          <Searchbar
+            style={styles.searchBar}
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
           <StatusBar style="auto" />
         </View>
-        <View style={styles.container2}>
-          <Text>List is here</Text>
+        <View style={styles.List}>
+          <Text>List</Text>
         </View>
         <StatusBar style="auto" />
       </SafeAreaView>
@@ -21,13 +29,16 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container1: {
+  search: {
     backgroundColor: "lightgreen",
     padding: 16,
   },
-  container2: {
+  List: {
     flex: 1,
     padding: 16,
     backgroundColor: "lightblue",
+  },
+  searchBar: {
+    borderRadius: 7,
   },
 });
